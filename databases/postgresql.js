c6220926d7bf30b55ca65de817a804553fa111b8
jererodri14 <jererodri14@gmail.com>
2023-07-22 16:29:17 -0600

    Se crea la tabla transaction

--- databases/postgresql.js
+++ databases/postgresql.js
@@ -5,6 +5,8 @@ const postgresql = newDb();
 // create mock data
 postgresql.public.none(`create table users(pk_user integer, name text, status boolean);
                 insert into users values (123, 'Juan', true);`);
+
+postgresql.public.none(`create table transaction(pk_transaction integer, fk_user integer, description text, amount decimal)`);
                 
 module.exports = {
     postgresql
