7e86b9dbd1eaa472ad9faacd750c657ed86c3081
jererodri14 <jererodri14@gmail.com>
2023-07-23 13:06:57 -0600

    Se valida si el user existe y esta activo

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -4,7 +4,10 @@ const createTransaction = async (req, res, next) => {
     const { pk_transaction, fk_user, description, amount } = req.body
     try {
         let transaction = await transactions.createTransaction(pk_transaction, fk_user, description, amount)
-        res.status(200).send(transaction)
+        if (transaction instanceof Error)
+            res.status(400).send(transaction.message)
+        else
+            res.status(200).send(transaction)
         next()
     } catch (e) {
         console.log(e.message)
